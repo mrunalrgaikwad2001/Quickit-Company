309510c86fd366339ff8a0a85be670b58afc43e5
mrunalrgaikwad2001 <mrunalrgaikwad2001@gmail.com>
2025-04-10 17:41:18 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,11 @@
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
-
+const {Client}=pkg;
 const database = new pg.Client({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT||5433
+
     connectionString:process.env.DATABASE_URL,ssl:{
-    rejectUnauthorized:false}
+    rejectUnauthorized:false,},
 });
 
 database.connect((err)=>{
